caf515aa10be5bddf8b2df2c77b8a0c878fb03f9
Travis <thoyle3293@gmail.com>
2017-08-28 15:18:41 -0700

    change2

--- wwwroot/ngApp/controllers/mainController.js
+++ wwwroot/ngApp/controllers/mainController.js
@@ -6,4 +6,16 @@ class MainController {
     return Math.floor((Math.random() * 10) + 1);
     
     }
+//create array of 10 fortunes
+//creat function to randomly pick one fortunes
+// createArray() {
+//     return [];
+//     let array = ["So text", "Going to work on this", "Keep working", "Use your resources", "Note to self", "If others can do it so can you", "Study like your job depends on it, cause it does"]
+// }
+
+getFortune(fortunes){
+    return "";
+}
+
+
 }
