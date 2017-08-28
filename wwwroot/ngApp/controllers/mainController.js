5a46aa8305748d5fa795cc907b90ffd34b1adf1c
Travis <thoyle3293@gmail.com>
2017-08-28 15:10:15 -0700

    Array's

--- wwwroot/ngApp/controllers/mainController.js
+++ wwwroot/ngApp/controllers/mainController.js
@@ -1,5 +1,9 @@
 class MainController {
     constructor() {
-        this.message = "hello from main";
+        this.message = this.randomFortune();
+    }
+    randomFortune(){
+    return Math.floor((Math.random() * 10) + 1);
+    
     }
 }
